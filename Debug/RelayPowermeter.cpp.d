cc1e92c9d4c64df5bda217c04fe06ff79fcffd0e
nazmang <nazmang@gmail.com>
2017-08-27 23:40:35 +0300

    Added MySensor support

---
+++ Debug/RelayPowermeter.cpp.d
@@ -0,0 +1,57 @@
+d:\Users\nazman\AppData\Local\Temp\VMBuilds\RelayPowermeter\pro_16MHzatmega328\Debug\RelayPowermeter.cpp.o: \
+ d:\Users\nazman\AppData\Local\Temp\VMBuilds\RelayPowermeter\pro_16MHzatmega328\Debug\RelayPowermeter.cpp \
+ d:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ d:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
+ d:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ d:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
+ d:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ d:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ d:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ d:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
+ d:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ d:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ d:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\eightanaloginputs/pins_arduino.h \
+ d:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\eightanaloginputs/../standard/pins_arduino.h \
+ d:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\SPI\src/SPI.h \
+ d:\Users\nazman\Documents\Arduino\libraries\MySensors/MySensors.h \
+ d:\Users\nazman\Documents\Arduino\libraries\MySensors/core/MySensorsCore.h \
+ d:\Users\nazman\Documents\Arduino\libraries\MySensors/core/Version.h \
+ d:\Users\nazman\Documents\Arduino\libraries\MySensors/MyConfig.h \
+ d:\Users\nazman\Documents\Arduino\libraries\MySensors/core/MyEepromAddresses.h \
+ d:\Users\nazman\Documents\Arduino\libraries\MySensors/core/MyMessage.h \
+ d:\Users\nazman\Documents\Arduino\libraries\MySensors/drivers/AVR/DigitalWriteFast/digitalWriteFast.h \
+ d:\Users\nazman\Documents\Arduino\libraries\MySensors/core/MyHwAVR.cpp \
+ d:\Users\nazman\Documents\Arduino\libraries\MySensors/core/MyHwAVR.h \
+ d:\Users\nazman\Documents\Arduino\libraries\MySensors/core/MyHw.h \
+ d:\Users\nazman\Documents\Arduino\libraries\MySensors/core/MyLeds.h \
+ d:\Users\nazman\Documents\Arduino\libraries\MySensors/core/MyIndication.cpp \
+ d:\Users\nazman\Documents\Arduino\libraries\MySensors/core/MyIndication.h \
+ d:\Users\nazman\Documents\Arduino\libraries\MySensors/core/MySigning.cpp \
+ d:\Users\nazman\Documents\Arduino\libraries\MySensors/core/MySigning.h \
+ d:\Users\nazman\Documents\Arduino\libraries\MySensors/core/MySensorsCore.h \
+ d:\Users\nazman\Documents\Arduino\libraries\MySensors/drivers/ATSHA204/ATSHA204.h \
+ d:\Users\nazman\Documents\Arduino\libraries\MySensors/drivers/ATSHA204/sha256.h \
+ d:\Users\nazman\Documents\Arduino\libraries\MySensors/drivers/ATSHA204/sha256.cpp \
+ d:\Users\nazman\Documents\Arduino\libraries\MySensors/drivers/ATSHA204/sha256.h \
+ d:\Users\nazman\Documents\Arduino\libraries\MySensors/core/MyTransport.cpp \
+ d:\Users\nazman\Documents\Arduino\libraries\MySensors/core/MyTransport.h \
+ d:\Users\nazman\Documents\Arduino\libraries\MySensors/drivers/RF24/RF24.cpp \
+ d:\Users\nazman\Documents\Arduino\libraries\MySensors/drivers/RF24/RF24.h \
+ d:\Users\nazman\Documents\Arduino\libraries\MySensors/core/MyTransportNRF24.cpp \
+ d:\Users\nazman\Documents\Arduino\libraries\MySensors/drivers/RF24/RF24.h \
+ d:\Users\nazman\Documents\Arduino\libraries\MySensors/drivers/CircularBuffer/CircularBuffer.h \
+ d:\Users\nazman\Documents\Arduino\libraries\MySensors/core/MyCapabilities.h \
+ d:\Users\nazman\Documents\Arduino\libraries\MySensors/core/MyMessage.cpp \
+ d:\Users\nazman\Documents\Arduino\libraries\MySensors/core/MySensorsCore.cpp \
+ d:\Users\nazman\Documents\Arduino\libraries\MySensors/core/MyMainDefault.cpp \
+ d:\Users\nazman\Documents\Arduino\libraries\Bounce2/Bounce2.h \
+ d:\Users\nazman\Documents\Arduino\libraries\Time/TimeLib.h \
+ d:\Users\nazman\Documents\Arduino\libraries\DS3232RTC/DS3232RTC.h \
+ d:\Users\nazman\Documents\Arduino\libraries\Time/Time.h \
+ d:\Users\nazman\Documents\Arduino\libraries\Time/TimeLib.h \
+ d:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\Wire\src/Wire.h \
+ d:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ d:\Users\nazman\Documents\Arduino\libraries\NewliquidCrystal/LiquidCrystal_I2C.h \
+ d:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ d:\Users\nazman\Documents\Arduino\libraries\NewliquidCrystal/I2CIO.h \
+ d:\Users\nazman\Documents\Arduino\libraries\NewliquidCrystal/LCD.h
